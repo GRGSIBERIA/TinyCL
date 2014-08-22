4546e491b629c606c2d0748ac67464f209dd29db
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-08-22 18:00:49 +0900

    自身を返すプロパティを追加

--- CLWorkGroupSettings.hpp
+++ CLWorkGroupSettings.hpp
@@ -53,9 +53,27 @@ namespace tcl
 			return workDimension;
 		}
 
+		/**
+		* ワーカーの次元数を設定する
+		*/
+		inline CLWorkGroupSettings& Dimension(const cl_uint dimention)
+		{
+			workDimension = dimention;
+			return *this;
+		}
+
 		inline size_t* WorkerSize()
 		{
 			return &globalWorker[0];
+		}
+
+		/**
+		* ワーカーの数を設定する
+		*/
+		inline CLWorkGroupSettings& WorkerSize(const std::vector<size_t>& workerSize)
+		{
+			globalWorker = std::vector<size_t>(workerSize);
+			return *this;
 		}
 
 		inline size_t* Offset()
@@ -63,9 +81,27 @@ namespace tcl
 			return &globalOffset[0];
 		}
 
+		/**
+		* ワーカーの初期実行位置を設定する
+		*/
+		inline CLWorkGroupSettings& Offset(const std::vector<size_t>& offset)
+		{
+			globalOffset = std::vector<size_t>(offset);
+			return *this;
+		}
+
 		inline size_t* SplitSize()
 		{
 			return &localWorker[0];
+		}
+
+		/**
+		* ワーカーを小分けする数を設定する
+		*/
+		inline CLWorkGroupSettings& SplitSize(const std::vector<size_t>& splitSize)
+		{
+			localWorker = std::vector<size_t>(splitSize);
+			return *this;
 		}
 
 		CLWorkGroupSettings& Optimize(const CLDeviceInformation& device)
@@ -85,15 +121,15 @@ namespace tcl
 		* \param[in] offset 次元数に応じた実行位置
 		* \param[in] splitSize ワークアイテム全体の区切り方
 		*/
-		CLWorkGroupSettings(const cl_uint dimension, const std::vector<size_t>& offset, const std::vector<size_t>& workerSize, const std::vector<size_t>& splitSize)
+		CLWorkGroupSettings(const cl_uint dimension = 0, const std::vector<size_t>& offset = {}, const std::vector<size_t>& workerSize = {}, const std::vector<size_t>& splitSize = {})
 			:
 			workDimension(dimension),
 			globalWorker(workerSize),
 			globalOffset(offset),
 			localWorker(splitSize)
 		{
-			if (workerSize.size() != dimension || splitSize.size() != dimension || offset.size() != dimension)
-				throw CLException("与えられた次数に対して，与えられた引数の長さが大きすぎるか小さすぎます");
+			if (workerSize.size() > dimension || splitSize.size() > dimension || offset.size() > dimension)
+				throw CLException("与えられた次数に対して，与えられた引数の長さが小さすぎます");
 		}
 	};
 }
