c948740f0fe61b01ac24ef00c3de5fcd9ec28f71
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-08-04 23:46:20 +0900

    CLFFT用のコントローラ追加します

---
+++ CLFFTController.hpp
@@ -0,0 +1,14 @@
+#ifndef CL_FFT_CONTROLLER_HPP
+#define CL_FFT_CONTROLLER_HPP
+
+#include "CLController.hpp"
+
+namespace tcl
+{
+	class CLFFTController : public CLController
+	{
+		
+	};
+}
+
+#endif
