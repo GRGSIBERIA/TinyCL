a84fb346e8a3cb1dad8ced7ac50b4a2ffdfbeb21
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-08-04 20:57:55 +0900

    ちょびちょび変更

--- TinyCL/CLController.hpp
+++ TinyCL/CLController.hpp
@@ -11,12 +11,16 @@ namespace tcl
 		CLMachineInformation machineInfo;
 
 	public:
-		inline const CLMachineInformation& Machine() const { return machineInfo; }
+		inline const CLMachineInformation& Machine() { return machineInfo; }
 
-		
-
+		inline const cl_int GetNumberOfDevices() const { return machineInfo.numDevices; }
+		inline const cl_int GetNumberOfPlatforms() const { return machineInfo.numPlatforms; }
 		inline cl_context GetContext() { return machineInfo.context; }
+
+		__declspec(property(get = GetNumberOfDevice)) cl_int NumberOfDevices;
+		__declspec(property(get = GetNumberOfPlatforms)) cl_int NumberOfPlatforms;
 		__declspec(property(get = GetContext)) cl_context Context;
+
 	};
 }
 
